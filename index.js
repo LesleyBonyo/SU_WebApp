9e1f86aa5fb8de1fdaa1e36ee4f9d8ad1d38a765
LesleyBonyo <lesleybonyo@gmail.com>
2022-01-07 12:37:12 +0300

    SU WebApp

---
+++ index.js
@@ -0,0 +1,14 @@
+// my first comment
+console.log("hello world");
+/* multi
+line
+comment*/
+var num = 2;
+var myName = "lesley"
+
+var greeting;
+greeting = "Hi, my name is" + myName + ". What's your name";
+num = greeting.length;
+console.log(num);
+
+console.log(greeting);

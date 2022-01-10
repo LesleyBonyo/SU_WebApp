d87ef57483c7774e3f1b3c054e39f808d30b9fd8
LesleyBonyo <lesleybonyo@gmail.com>
2022-01-10 09:05:16 +0300

    SU Webapp

---
+++ navigation/index.js
@@ -0,0 +1,36 @@
+//console.log('Hello World')
+
+//let name = 'lesley';
+//let weight = 54.2;
+/*let firstName = undefined;
+let lastName = null;
+let gender = false;
+console.log(name);
+
+const age = 13;
+
+console.log(age);
+*/
+//objects
+let details = {
+	name:'lesley',
+	age: 25
+}
+details.name = 'Jane'
+console.log(details.name);
+console.log(details);
+
+//arrays
+let fruits = ['orange', 'banana'];
+console.log(fruits);
+
+//function
+function greet(){
+	console.log("hello world");
+}
+greet();
+function greetSomeone(name) {
+	console.log("Hi" + name);
+	// body...
+}
+greetSomeone('John');
